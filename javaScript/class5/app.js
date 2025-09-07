c11507bfc2890ba48347fa4e0ee0d155f37fae98
Shahbaz Ahmed <ashabaz845@gmail.com>
2025-09-07 16:15:41 +0500

    Code update

---
+++ javaScript/class5/app.js
@@ -0,0 +1,71 @@
+//Chapter 1
+ // Question 1
+//alert("Hello")
+                //0         1    2   3
+// var FriendList = ["Hamza","Umer",12,true]
+
+// console.log("Array Create",FriendList[5])
+// console.log(FriendList.length)
+
+// FriendList[1] = "SHahbaz"
+
+// console.log(FriendList)
+
+// FriendList.push("Mobile","Laptop",12,23)
+
+// console.log(FriendList)
+
+
+// FriendList.pop()
+
+// console.log(FriendList)
+
+
+// FriendList.unshift("Samsung","Motorolla","Iphone")
+
+// console.log(FriendList)
+
+
+// FriendList.shift()
+
+// console.log(FriendList)
+
+
+var FriendList = ["Hamza","Umer","Samsung","Ihpone",12,true]
+
+// FriendList.splice(2,1,"Dell","Hp")
+
+
+
+var FriendListCopy = FriendList.slice(1,4)
+
+console.log(FriendList)
+
+console.log(FriendListCopy)
+
+
+console.log(FriendList.indexOf("s"))
+
+console.log(FriendList.includes("Samsungs"))
+
+// console.log(FriendList.join(","))
+
+// var userName = "Shahbaz Ahmed     "
+// var userNameTrim = userName.trim()
+
+// console.log(userName.length) //18
+
+// console.log(userNameTrim.length)  
+
+
+
+
+
+
+
+
+
+
+
+
+
